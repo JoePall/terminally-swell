d478bed8026f30bc39ff3b1bc967b01acc055abb
Josiah Powell <josiahpowell@outlook.com>
2020-08-12 09:23:55 -0500

    Init

---
+++ extension.js
@@ -0,0 +1,43 @@
+const vscode = require('vscode');
+const path = require('path');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	let disposable = vscode.commands.registerCommand('terminally-swell.terminallySwell', function () {
+
+		if (!vscode.window.activeTextEditor) return;
+		if (!vscode.window.activeTextEditor.document) return;
+		if (!vscode.window.activeTextEditor.document.uri) return;
+		if (path.extname(vscode.window.activeTextEditor.document.uri.fsPath) !== ".js") return;
+
+		if (vscode.window.terminals.length > 0) {
+			let existingTerminal = vscode.window.terminals[0];
+
+			existingTerminal.sendText("cd " + path.dirname(vscode.window.activeTextEditor.document.uri.fsPath));
+			existingTerminal.sendText("cls");
+			existingTerminal.sendText("node " + path.basename(vscode.window.activeTextEditor.document.uri.fsPath));
+			
+			return;
+		}
+
+		var terminal = vscode.window.createTerminal({
+			cwd: path.dirname(vscode.window.activeTextEditor.document.uri.fsPath)
+		});
+		
+		terminal.sendText("node " + path.basename(vscode.window.activeTextEditor.document.uri.fsPath));
+		
+		terminal.show(false);
+	});
+
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
