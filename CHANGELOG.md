60bf519e42d937b0b147691449fc47a702472691
Josiah Powell <josiahpowell@outlook.com>
2020-08-13 21:21:19 -0500

    Built open in terminal

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to the "terminally-swell" extension will be documented in this file.
 
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+
 
 ## 0.7.0 - 8/13/20
 
--- extension.js
+++ extension.js
@@ -1,45 +1,83 @@
-const vscode = require('vscode');
-const path = require('path');
+const vscode = require("vscode");
+const path = require("path");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('terminally-swell.terminallySwell', function () {
+  let disposable = vscode.commands.registerCommand(
+    "terminally-swell.terminalHere",
+    function () {
+      if (!vscode.window.activeTextEditor) return;
+      if (!vscode.window.activeTextEditor.document) return;
+      if (!vscode.window.activeTextEditor.document.uri) return;
 
-		if (!vscode.window.activeTextEditor) return;
-		if (!vscode.window.activeTextEditor.document) return;
-		if (!vscode.window.activeTextEditor.document.uri) return;
-		if (path.extname(vscode.window.activeTextEditor.document.uri.fsPath)  !== ".js") return;
-
-		if (vscode.window.terminals.length > 0) {
-			let existingTerminal = vscode.window.terminals[0];
-
-			existingTerminal.sendText("cd " + path.dirname(vscode.window.activeTextEditor.document.uri.fsPath));
-			existingTerminal.sendText("cls");
-			existingTerminal.sendText("node " + path.basename(vscode.window.activeTextEditor.document.uri.fsPath));
-
-			existingTerminal.show(false);
-			
-			return;
-		}
-
-		var terminal = vscode.window.createTerminal({
-			cwd: path.dirname(vscode.window.activeTextEditor.document.uri.fsPath)
-		});
-		
-		terminal.sendText("node " + path.basename(vscode.window.activeTextEditor.document.uri.fsPath));
-		
+      if (vscode.window.terminals.length > 0) {
+		let terminal = vscode.window.terminals[0];
 		terminal.show(false);
-	});
+	  }
+	  else {
+		  var terminal = vscode.window.createTerminal({
+			cwd: path.dirname(vscode.window.activeTextEditor.document.uri.fsPath),
+		  });
 
-	context.subscriptions.push(disposable);
+		  terminal.show(false);
+	  }
+    }
+  );
+
+  context.subscriptions.push(disposable);
+
+  disposable = vscode.commands.registerCommand(
+    "terminally-swell.runInNode",
+    function () {
+      if (!vscode.window.activeTextEditor) return;
+      if (!vscode.window.activeTextEditor.document) return;
+      if (!vscode.window.activeTextEditor.document.uri) return;
+      if (
+        path.extname(vscode.window.activeTextEditor.document.uri.fsPath) !==
+        ".js"
+      )
+        return;
+
+      if (vscode.window.terminals.length > 0) {
+        let existingTerminal = vscode.window.terminals[0];
+
+        existingTerminal.sendText(
+          "cd " +
+            path.dirname(vscode.window.activeTextEditor.document.uri.fsPath)
+        );
+        existingTerminal.sendText("cls");
+        existingTerminal.sendText(
+          "node " +
+            path.basename(vscode.window.activeTextEditor.document.uri.fsPath)
+        );
+
+        existingTerminal.show(false);
+
+        return;
+      }
+
+      var terminal = vscode.window.createTerminal({
+        cwd: path.dirname(vscode.window.activeTextEditor.document.uri.fsPath),
+      });
+
+      terminal.sendText(
+        "node " +
+          path.basename(vscode.window.activeTextEditor.document.uri.fsPath)
+      );
+
+      terminal.show(false);
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-function deactivate() { }
+function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
